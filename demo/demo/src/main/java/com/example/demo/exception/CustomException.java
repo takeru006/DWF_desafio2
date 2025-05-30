c817ae1812f7495a1592432097ce466ab9e7fd1f
Justin0477 <132772383+takeru006@users.noreply.github.com>
2025-05-29 20:13:59 -0600

    Delete demo/demo directory

--- demo/demo/src/main/java/com/example/demo/exception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class CustomException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public CustomException(String message) {
-        super(message);
-    }
-}
